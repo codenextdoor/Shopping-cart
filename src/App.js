4e82d76396e684705fb1ee9657d9167ee0f6c4ac
codenextdoor <xuluv911@gmail.com>
2022-12-27 12:09:18 +0545

    Navbar commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
+import Cart from "./pages/cart/Cart";
+
+import Shop from "./pages/shop/Shop";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>CND SHOP</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
