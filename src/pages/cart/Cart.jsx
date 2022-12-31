9ba1e8d3db311e3feccb3dc6ff608ac1c8b0b0f7
codenextdoor <xuluv911@gmail.com>
2022-12-31 15:09:31 +0545

    styling the cart

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import { PRODUCTS } from "../../Products";
+import { ShopContext } from "../../context/ShopContext";
+import CartItem from "./CartItem";
+import "./cart.css";
 
 const Cart = () => {
+  const { cartItems } = useContext(ShopContext);
   return (
     <div className="cart">
-      <h1> Your cart items </h1>
-      <div className="cartItems"></div>
+      <div>
+        <h1> Your cart items </h1>
+      </div>
+      <div className="cartItems">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] !== 0) {
+            return <CartItem data={product} />;
+          }
+        })}
+      </div>
     </div>
   );
 };
